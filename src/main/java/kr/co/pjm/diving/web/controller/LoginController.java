5e5c1bedfd1193cf119d3ff6107f794b0d0d6683
jmpark <kbtapjm@gmail.com>
2017-07-16 23:58:43 +0900

    #2 시큐리티로그인 메소드 생성

--- src/main/java/kr/co/pjm/diving/web/controller/LoginController.java
+++ src/main/java/kr/co/pjm/diving/web/controller/LoginController.java
@@ -1,14 +1,20 @@
 package kr.co.pjm.diving.web.controller;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -48,6 +54,18 @@ public class LoginController {
     }
 
     return "redirect:/";
+  }
+  
+  /**
+   * 로그인 처리(API형식)
+   * @return
+   */
+  @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseBody @ResponseStatus(HttpStatus.OK)
+  public Map<String, Object> login() {
+    Map<String, Object> resultMap = new HashMap<String, Object>();
+    
+    return resultMap;
   }
 
 }
