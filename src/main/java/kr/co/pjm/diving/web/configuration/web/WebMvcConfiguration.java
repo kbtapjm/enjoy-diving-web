edddc6ab74e3d075f8ad451cd38b2c38bf0b1ae1
kbtapjm <kbtapjm@gmail.com>
2018-08-27 17:05:27 +0900

    added application.yml value encypt source

--- src/main/java/kr/co/pjm/diving/web/configuration/web/WebMvcConfiguration.java
+++ src/main/java/kr/co/pjm/diving/web/configuration/web/WebMvcConfiguration.java
@@ -2,6 +2,9 @@ package kr.co.pjm.diving.web.configuration.web;
 
 import java.util.Locale;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -16,8 +19,8 @@ import kr.co.pjm.diving.web.common.security.AuthAndDateAuditorAware;
 
 /**
  * <pre>
- * @Package Name : kr.co.pjm.diving.web.configuration.web
- * @Class Name : WebMvcConfiguration.java
+ * &#64;Package Name : kr.co.pjm.diving.web.configuration.web
+ * &#64;Class Name : WebMvcConfiguration.java
  * </pre>
  * 
  * @author : jmpark
@@ -36,19 +39,19 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
     sessionLocaleResolver.setDefaultLocale(Locale.KOREA);
     return sessionLocaleResolver;
   }
-  
+
   @Bean
   public LocaleChangeInterceptor localeChangeInterceptor() {
     LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
     localeChangeInterceptor.setParamName("language");
     return localeChangeInterceptor;
   }
-  
+
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(localeChangeInterceptor());
   }
-  
+
   @Bean
   public AuthAndDateAuditorAware auditorAware() {
     return new AuthAndDateAuditorAware();
@@ -56,13 +59,25 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry
-      .addMapping("/**")
-      .allowedOrigins("*")
-      .allowedMethods("POST", "GET",  "PUT", "DELETE")
-      .allowedHeaders("Access-Control-Allow-Headers", "Content-Type", "x-requested-with")
-      .allowCredentials(false)
-      .maxAge(1);
+    registry.addMapping("/**").allowedOrigins("*").allowedMethods("POST", "GET", "PUT", "DELETE")
+        .allowedHeaders("Access-Control-Allow-Headers", "Content-Type", "x-requested-with").allowCredentials(false)
+        .maxAge(1);
   }
-  
+
+  @Bean("jasyptStringEncryptor")
+  public StringEncryptor stringEncryptor() {
+    PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+    SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+    config.setPassword("test"); // 암호화에 사용할 키 -> 중요
+    config.setAlgorithm("PBEWithMD5AndDES"); // 사용할 알고리즘
+    config.setKeyObtentionIterations("1000");
+    config.setPoolSize("1");
+    config.setProviderName("SunJCE");
+    config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+    config.setStringOutputType("base64");
+    encryptor.setConfig(config);
+
+    return encryptor;
+  }
+
 }
---
+++ src/test/java/kr/co/pjm/diving/web/service/EnjoyDivingTest.java
@@ -0,0 +1,21 @@
+package kr.co.pjm.diving.web.service;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+public class EnjoyDivingTest {
+
+  public static void main(String[] args) {
+    StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+    pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+    pbeEnc.setPassword("test"); //2번 설정의 암호화 키를 입력
+    
+    String enc = pbeEnc.encrypt("jdbc:mysql://localhost:3306/diving"); //암호화 할 내용
+    System.out.println("enc = " + enc); //암호화 한 내용을 출력
+    
+    //테스트용 복호화
+    String des = pbeEnc.decrypt(enc);
+    System.out.println("des = " + des);
+
+  }
+
+}
