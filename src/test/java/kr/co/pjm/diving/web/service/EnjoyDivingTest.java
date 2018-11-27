6aee92f2276678575483c8088b014972d6bce99e
jmpark <kbtapjm@gmail.com>
2018-11-28 08:05:56 +0900

    encryt facebook app, secret id

--- src/test/java/kr/co/pjm/diving/web/service/EnjoyDivingTest.java
+++ src/test/java/kr/co/pjm/diving/web/service/EnjoyDivingTest.java
@@ -9,7 +9,8 @@ public class EnjoyDivingTest {
     pbeEnc.setAlgorithm("PBEWithMD5AndDES");
     pbeEnc.setPassword("test"); //2번 설정의 암호화 키를 입력
     
-    String enc = pbeEnc.encrypt("jdbc:mysql://localhost:3306/diving"); //암호화 할 내용
+    //String enc = pbeEnc.encrypt("jdbc:mysql://localhost:3306/diving"); //암호화 할 내용
+    String enc = pbeEnc.encrypt("dae737184554557de33db0cb4d8c1923"); //암호화 할 내용
     System.out.println("enc = " + enc); //암호화 한 내용을 출력
     
     //테스트용 복호화
